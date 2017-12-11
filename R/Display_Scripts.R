534047d2641c7860b4d3010fdc14457f818dfc55
simcor06 <chertog@clarku.edu>
2017-12-11 10:37:32 -0500

    eliminated uneccesary plot things

--- R/Display_Scripts.R
+++ R/Display_Scripts.R
@@ -30,18 +30,8 @@ plot(state_tracts, col = "grey", border = "red", add = TRUE)
 plot(state_upper[state_upper@data$SLDUST == 1,],
      border = "transparent", col = rgb(0, 0, 1, .4), add = TRUE)
 
-### DOESN"T WORK#### Population by district Plotting using sp package
-par(mfrow = c(1, 3))
-pal <- brewer.pal(5, "YlOrRd")
-for(i in 1:length(districts_pop)) {
-  breaks_qt <- classIntervals(districts_pop[[i]]@data$Pop_Total, n = 5)
-  br <- breaks_qt$brks
-  offs <- 0.0000001
-  br[1] <- br[1] - offs
-  br[length(br)] <- br[length(br)] + offs
-  districts_pop[[i]]@data$Pop_Total_R_bracket <- cut(districts_pop[[i]]@data$Pop_Total, br)
-  spplot(districts_pop[[i]], "Pop_Total_R_bracket", col.regions = pal)
-}
+
+
 
 
 ## displaying upper District Total Populations
@@ -76,13 +66,6 @@ br[length(br)] <- br[length(br)] + offs
 sfcd_pop@data$Pop_Black_R_bracket <- cut(sfcd_pop@data$Pop_Black, br)
 spplot(sfcd_pop, "Pop_Black_R_bracket", main = "Federal Congressional District Black Populations", col.regions = pal)
 
-###  DOESN't WORK### Using GISTools Package
-shd <-  auto.shading(sud_pop@data$Pop_Total)
-choro.legend(bbox(sud_pop)["x","max"] -300000, bbox(sud_pop)["y","min"]+ 300000  , shd)
-title("Wisconsin State Upper House District Populations")
-choropleth(sud_pop, sud_pop@data$Pop_Total)
-bbox(sud_pop)["x","max"]
-bbox(sud_pop)["y","min"]
 
 
 
