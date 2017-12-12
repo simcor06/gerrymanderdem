a66736fac0d366b72dc7a325755d2e81cff99d42
simcor06 <chertog@clarku.edu>
2017-12-12 14:07:24 -0500

    added the import form code at the top of each script to put it in NAMESPACE FILE

--- R/North_Carolina.R
+++ R/North_Carolina.R
@@ -5,6 +5,7 @@
 #' @export
 #' @examples
 #' gerrymanderdem
+#' @importFrom (GenomicRanges, as.data.frame)
 
 library(tigris)
 library(tidycensus)
@@ -44,26 +45,26 @@ map_proj <- get_proj4(3404) #### insert a ESPG code, currently a Transverse merc
 ###################################################################################################
 
 #State Census Tracts
-state_tracts_NAD83<-tracts(state = FIPS, cb= TRUE)
-state_tracts<-spTransform(state_tracts_NAD83, CRS(aea_US))
-state_tracts@data$AREA<-gArea(state_tracts, byid = TRUE)
+state_tracts_NAD83 <- tracts(state = FIPS, cb= TRUE)
+state_tracts <- spTransform(state_tracts_NAD83, CRS(aea_US))
+state_tracts@data$AREA <- gArea(state_tracts, byid = TRUE)
 
 #State Lower House Districts
-state_lower_NAD83<-state_legislative_districts(state = FIPS, house = "lower", cb = TRUE,
+state_lower_NAD83 <- state_legislative_districts(state = FIPS, house = "lower", cb = TRUE,
                                                year = NULL)
-state_lower<-spTransform(state_lower_NAD83, CRS(aea_US))
+state_lower <- spTransform(state_lower_NAD83, CRS(aea_US))
 state_lower@data$SLDLST <- as.numeric(state_lower@data$SLDLST)
 
 #State Upper House Districts
-state_upper_NAD83<-state_legislative_districts(state = FIPS, house = "upper", cb = TRUE,
+state_upper_NAD83 <- state_legislative_districts(state = FIPS, house = "upper", cb = TRUE,
                                                year = NULL)
-state_upper<-spTransform(state_upper_NAD83, CRS(aea_US))
+state_upper <- spTransform(state_upper_NAD83, CRS(aea_US))
 state_upper@data$SLDUST <- as.numeric(state_upper@data$SLDUST)
 
 #Federal Congressional Districts at state level
-fed_congress_NAD83<-congressional_districts(cb = TRUE, year = NULL)
+fed_congress_NAD83 <- congressional_districts(cb = TRUE, year = NULL)
 fed_congress_state_NAD83 <- fed_congress_NAD83[fed_congress_NAD83@data$STATEFP == FIPS,]
-fed_congress_state<-spTransform(fed_congress_state_NAD83, CRS(aea_US))
+fed_congress_state <- spTransform(fed_congress_state_NAD83, CRS(aea_US))
 fed_congress_state@data$CD114FP <- as.numeric(fed_congress_state@data$CD114FP)
 
 
@@ -72,23 +73,23 @@ fed_congress_state@data$CD114FP <- as.numeric(fed_congress_state@data$CD114FP)
 ###### FOLLOWING ARE DEMOGRAPHIC-RELATED OPERATIONS #######
 
 
-all<-get_decennial(geography = "tract", variables = "P0100001", state= FIPS, year=2010)
-White_over18<-get_decennial(geography = "tract", variables ="P016A003",state = FIPS, year = 2010)
-Black_over18<-get_decennial(geography = "tract", variables ="P016B003",state = FIPS, year = 2010)
-American_Indian_over18<-get_decennial(geography = "tract", variables ="P036C003",state = FIPS, year = 2010)
-Asian_over18<-get_decennial(geography = "tract", variables ="P016D003",state = FIPS, year = 2010)
-Pacific_Islander_over18<-get_decennial(geography = "tract", variables ="P036E003",state = FIPS, year = 2010)
-Hispanic_over18<-get_decennial(geography = "tract", variables ="P016H003",state = FIPS, year = 2010)
+all <- get_decennial(geography = "tract", variables = "P0100001", state= FIPS, year=2010)
+White_over18 <- get_decennial(geography = "tract", variables ="P016A003",state = FIPS, year = 2010)
+Black_over18 <- get_decennial(geography = "tract", variables ="P016B003",state = FIPS, year = 2010)
+American_Indian_over18 <- get_decennial(geography = "tract", variables ="P036C003",state = FIPS, year = 2010)
+Asian_over18 <- get_decennial(geography = "tract", variables ="P016D003",state = FIPS, year = 2010)
+Pacific_Islander_over18 <- get_decennial(geography = "tract", variables ="P036E003",state = FIPS, year = 2010)
+Hispanic_over18 <- get_decennial(geography = "tract", variables ="P016H003",state = FIPS, year = 2010)
 
 
 
 
 #cbind
 
-Universe_18<- cbind(all,White_over18$value,Black_over18$value,American_Indian_over18$value,Asian_over18$value,Pacific_Islander_over18$value,Hispanic_over18$value)
+Universe_18 <- cbind(all,White_over18$value,Black_over18$value,American_Indian_over18$value,Asian_over18$value,Pacific_Islander_over18$value,Hispanic_over18$value)
 
 #change column names
-names(Universe_18)[4:10]<-c("Pop_Total","Pop_White","Pop_Black","Pop_American_Indian","Pop_Asian","Pop_Hawaiian_Pacific_Islander","Pop_Hispanic")
+names(Universe_18)[4:10] <- c("Pop_Total","Pop_White","Pop_Black","Pop_American_Indian","Pop_Asian","Pop_Hawaiian_Pacific_Islander","Pop_Hispanic")
 
 
 #merge census tracts df with demographics data
